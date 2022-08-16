b23264c26ee704a1f77478356415da3ecd0e28c4
aran-cho <oing9605@gmail.com>
2022-08-16 10:22:47 +0900

    commit

---
+++ Portfolio/Todolist_D.js
@@ -0,0 +1,63 @@
+$(function () {
+  $(".next").hide();
+  let inputD = document.querySelector("#inputDay");
+  let inputT = document.querySelector("#inputTxt");
+
+  $("#imgTxt>button").on("click", function () {
+    if (inputD.value === "" || inputT.value === "") {
+      alert("일기를 등록하세요.");
+    } else {
+      alert("일기가 등록되었습니다.");
+      $(".next").show();
+      document.querySelector("#nextDay").innerText = inputD.value;
+      document.querySelector("#nextTxt").innerText = inputT.value;
+    }
+    console.log(inputD.value);
+  });
+
+  //코드 줄여보기 (저번에 배운거 참고)
+  $(".btn2").hide();
+
+  $(".btn2").on("click", function () {
+    $("#txtImg1").slideDown(1000, function () {
+      $(".btn2").hide();
+      $(".btn1").show();
+    });
+  });
+  $(".btn1").on("click", function () {
+    $("#txtImg1").slideUp(1000, function () {
+      $(".btn1").hide();
+      $(".btn2").show();
+    });
+  });
+
+  $(".btn4").hide();
+
+  $(".btn4").on("click", function () {
+    $("#txtImg2").slideDown(1000, function () {
+      $(".btn4").hide();
+      $(".btn3").show();
+    });
+  });
+  $(".btn3").on("click", function () {
+    $("#txtImg2").slideUp(1000, function () {
+      $(".btn3").hide();
+      $(".btn4").show();
+    });
+  });
+
+  $(".btn6").hide();
+
+  $(".btn6").on("click", function () {
+    $("#txtImg3").slideDown(1000, function () {
+      $(".btn6").hide();
+      $(".btn5").show();
+    });
+  });
+  $(".btn5").on("click", function () {
+    $("#txtImg3").slideUp(1000, function () {
+      $(".btn5").hide();
+      $(".btn6").show();
+    });
+  });
+});
