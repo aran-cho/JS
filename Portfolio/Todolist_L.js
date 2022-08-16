52ea894f88a68b363d7bbe2fca24cc5f3c897531
aran-cho <oing9605@gmail.com>
2022-08-16 11:35:02 +0900

    commit

---
+++ Portfolio/Todolist_L.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('#inputText');
+let btn = document.querySelector('#addBtn');
+let listC =document.querySelector('#items')
+
+function add(){
+    
+}
+
+btn.addEventListener('click', add);
