a4d8329687cda6b36c55563b4328d4f1f15d2ce2
aran-cho <oing9605@gmail.com>
2022-08-17 11:04:21 +0900

    commit

--- Portfolio/Todolist_L.js
+++ Portfolio/Todolist_L.js
@@ -8,17 +8,42 @@ function add(e) {
   } else {
     let div = document.createElement("div");
     let divI = document.createAttribute("id");
-    divI.value = "divList";
-    div.setAttributeNode(divI);
-
     let p = document.createElement("p");
+    let span = document.createElement("span");
+    let check = document.createAttribute('class');
+    let txtCheck = document.createTextNode('√');
+    //ㄷ 한자
     let txt = document.createTextNode(inputText.value);
 
+    divI.value = 'divList';
+    check.value = 'check';
+
+    div.setAttributeNode(divI);
+    span.setAttributeNode(check);
+
+    // let img = document.createElement("img");
+    // let src = document.createAttribute('src');
+    // src.value = './image/check.png';
+    // img.setAttributeNode(src);
+
+    span.appendChild(txtCheck);
+    p.appendChild(span);
+    // div.appendChild(img);
     p.appendChild(txt);
     div.appendChild(p);
 
+    console.log(div);
+
     input.value = "";
 
+    //체크에 mouseover 하면 색변경
+    function color(){
+      txtCheck.style.color = 'red';
+    }
+
+    txtCheck.addEventListener('mouseover', color);
+    
+    // 체크하면 line-through
     document.querySelector("#list").appendChild(div);
 
     let click = document.querySelectorAll("p");
@@ -30,6 +55,8 @@ function add(e) {
         this.parentNode.style.textDecoration = "line-through";
       });
     }
+
+    //clear 누르면 사라짐
     let clear = document.querySelector('#clear');
     console.log(clear);
     
@@ -38,15 +65,7 @@ function add(e) {
     }
     clear.addEventListener('click', cle);
   }
-//   let clear = document.querySelector('#clear');
-
-//   function cle(){
-//     div.remove();
-// }
-// clear.addEventListener('click', cle);
 }
-
-
 
 btn.addEventListener("click", add);
 
